e13c807eb3b213c8ec806f7d33c7a8ba7ed4a4ab
totally-not-frito-lays <rlay@Area51>
2024-07-24 17:29:49 -0700

    removing old files

--- tools/11ty/_data/characters.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-module.exports = async () => {
-  const result = await axios.get("https://rickandmortyapi.com/api/character");
-  const characters = result.data.results;
-  return characters;
-}
--- tools/11ty/src/projects/projects.11tydata.js
+++
@@ -1,9 +0,0 @@
-// If we wanted to access dog.js from projects.11tydata.js, we would use:
-// const dogs = require("../../_data/dog");
-
-module.exports = async () => {
-  return {
-    layout: "layout.liquid",
-    tags: ["projects", "dog"]
-  };
-}
