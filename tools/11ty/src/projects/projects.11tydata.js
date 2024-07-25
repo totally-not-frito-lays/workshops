8bb43bdd0378f83bbadfab0369a3ad62841729d9
totally-not-frito-lays <rlay@Area51>
2024-07-24 17:17:16 -0700

    init example project from LWJ/11ty

---
+++ tools/11ty/src/projects/projects.11tydata.js
@@ -0,0 +1,9 @@
+// If we wanted to access dog.js from projects.11tydata.js, we would use:
+// const dogs = require("../../_data/dog");
+
+module.exports = async () => {
+  return {
+    layout: "layout.liquid",
+    tags: ["projects", "dog"]
+  };
+}
