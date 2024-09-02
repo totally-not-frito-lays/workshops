6d5700e75813716f0727e04fc81987617490c06d
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-09-02 13:54:35 +0930

    init 11ty blog files

--- content/tools/11ty/sandbox/_data/dog.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-module.exports = async () => {
-  const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  const image = result.data.message;
-  return image;
-}
